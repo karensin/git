154f6c6165dd11d86e2aa43a3f0dc6266d9dba85
Karen k  sin <karenksin@karens-air.gateway.sonic.net>
2018-11-20 00:14:21 -0800

    react-todo-app Events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import ToDo from './components/ToDo.js';
 import './App.css';
@@ -12,23 +11,65 @@ class App extends Component {
        { description: 'Walk the cat', isCompleted: true },
        { description: 'Throw the dishes away', isCompleted: false },
        { description: 'Buy new dishes', isCompleted: false }
-     ]
-   };
+     ],
+        newTodoDescription: ''
+    };
   }
+    handleChange(e) {
+      this.setState({ newTodoDescription: e.target.value })
+    }
 
+   handleSubmit(e) {
+     e.preventDefault();
+     if (!this.state.newTodoDescription) { return }
 
+     const newTodo = {
+        description: this.state.newTodoDescription, isCompleted: false
+      };
+     this.setState({
+       todos: [...this.state.todos, newTodo], newTodoDescription: '' }
+     );
+   }
 
+  toggleComplete(index) {
+     const todos = this.state.todos.slice();
+     const todo = todos[index];
+     todo.isCompleted = todo.isCompleted ? false : true;
+     this.setState({ todos: todos });
+   }
+
+   deleteItem = currentIndex => {
+       const filteredItems =  this.state.todos.filter((item, index) => {
+           return currentIndex !== index
+       })
+       this.setState({
+         todos: filteredItems,
+       })
+     }
 
   render() {
+
     return (
       <div className="App">
       <ul>
 
-              { this.state.todos.map( (todo, index) =>
-         <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+        { this.state.todos.map( (todo, index) =>
+          <ToDo
+            key={ index }
+            description={ todo.description }
+            isCompleted={ todo.isCompleted }
+            toggleComplete={ () => this.toggleComplete(index) }
+            deleteItem={() => this.deleteItem(index)}
+          />
+         )}
+      </ul>
 
-       )}
-            </ul>
+            <form onSubmit={ (e) => this.handleSubmit(e) }>
+            <input type="text" value={ this.state.newTodoDescription }
+            onChange={ (e) => this.handleChange(e) } />
+       <input type="submit" />
+     </form>
+
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,11 +3,20 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li>
-             <input type="checkbox" checked={ this.props.isCompleted } />
-             <span>{ this.props.description }</span>
-           </li>
-      );
+      <div>
+        <li>
+          <input
+            type="checkbox"
+            checked={ this.props.isCompleted }
+            onChange={ this.props.toggleComplete}
+          />
+          <button
+            onClick={this.props.deleteItem}
+          />
+          <span>{this.props.description}</span>
+        </li>
+      </div>
+    );
   }
 }
 
